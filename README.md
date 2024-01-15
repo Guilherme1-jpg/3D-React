e2d039bdca47184de4efd36a97b60ce4cff9ea68
Guilherme1-jpg <henriqueoliveiragui18@gmail.com>
2024-01-15 10:55:58 -0300

    feature: Background 3d and Avatar geometry

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## 3D - REACT
+Estudo sobre componentes 3d com react;
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
+import { SVGLoader } from 'three/addons/loaders/SVGLoader';
+import Thumb from './thumbnail.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [0, 0, 50] }} style={{ background: 'black' }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} intensity={1} />
+      <TorusKnot />
+      <Stars />
+    </Canvas>
   );
+}
+
+function TorusKnot() {
+  const ref = useRef();
+  const [hover, set] = useState(false);
+
+  const { paths } = useLoader(SVGLoader, Thumb);
+
+  return (
+    <group ref={ref}>
+      {paths.map((path, index) => (
+        <mesh
+          scale={[0.05, -0.05, 0.05]}
+          key={index}
+          position={[0, 0, 0]}
+          onPointerOver={() => set(true)}
+          onPointerOut={() => set(false)}
+        >
+          <extrudeGeometry args={[path.toShapes(true), { depth: 2, bevelEnabled: false }]} />
+          <meshBasicMaterial color={hover ? 'lightblue' : 'hotpink'} />
+        </mesh>
+      ))}
+    </group>
+  );
+}
+
+function Stars(props) {
+  const ref = useRef();
+  const positions = generateRandomStars(20000, 30);
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <Points ref={ref} positions={positions} stride={3} frustumCulled={false} {...props}>
+      <PointMaterial transparent color="#ffa0e0" size={0.02} sizeAttenuation={true} depthWrite={false} />
+    </Points>
+  );
+}
+
+// Função para gerar posições aleatórias uniformemente distribuídas
+function generateRandomStars(count, radius) {
+  const positions = [];
+
+  for (let i = 0; i < count; i++) {
+    const theta = Math.random() * Math.PI * 2;
+    const phi = Math.acos(Math.random() * 2 - 1);
+    const x = radius * Math.sin(phi) * Math.cos(theta);
+    const y = radius * Math.sin(phi) * Math.sin(theta);
+    const z = radius * Math.cos(phi);
+
+    positions.push(x, y, z);
+  }
+
+  return new Float32Array(positions);
 }
 
 export default App;
